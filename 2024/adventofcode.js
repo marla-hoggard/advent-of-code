3ebc0f5c9ca9d77a9fed0e0a8f506b92feb6d4d3
Marla Hoggard <marlabhoggard@gmail.com>
2024-12-01 15:03:58 -0500

    2024 day 1

---
+++ 2024/adventofcode.js
@@ -0,0 +1,57 @@
+/**
+ * Day 1 - Puzzle 1
+ *
+ * 1. Parse the input into two lists, one for each column
+ * 2. Sort both lists ascending
+ * 3. Compute the difference between left[i] and right[i] for each index
+ * 4. Return the sum of the differences
+ */
+const diffLists = (input) => {
+  const left = [];
+  const right = [];
+  input.split('\n').forEach((row) => {
+    const [l, r] = row.split(/\s+/).map(Number);
+    left.push(l);
+    right.push(r);
+  });
+  left.sort((a, b) => a - b);
+  right.sort((a, b) => a - b);
+
+  let diffs = 0;
+  for (let i = 0; i < left.length; i++) {
+    diffs += Math.abs(left[i] - right[i]);
+  }
+
+  return diffs;
+};
+
+/**
+ * Day 1 - Puzzle 2
+ * 1. Parse the input into two lists, one for each column
+ * 2. Determine how many times each element of left appears in right
+ * 3. Compute count * num for each element, and return the sum of each computation
+ */
+const similarityScore = (input) => {
+  const left = [];
+  const right = [];
+  input.split('\n').forEach((row) => {
+    const [l, r] = row.split(/\s+/).map(Number);
+    left.push(l);
+    right.push(r);
+  });
+
+  // Cache each value so we don't have to count occurrences more than once for a given value
+  let countMap = {};
+
+  let score = 0;
+  left.forEach((num) => {
+    if (countMap[num]) {
+      score += countMap[num];
+    } else {
+      const count = numOccurrences(right, num) * num;
+      countMap[num] = count;
+      score += count;
+    }
+  });
+  return score;
+};
